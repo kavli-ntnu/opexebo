f8f3e68ceb052b91c2866bef8865de937b144846
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2019-04-30 15:49:59 +0200

    RateMapStats, BorderScore added
    Initial work on rate map and border score.
    
    Currently untested, but I'm quite confident about RateMap Stats correctness. Not at all confident about border score, I just don't follow how it works.

---
+++ opexebo/analysis/borderCoverage.py
@@ -0,0 +1,113 @@
+"""Provide function for calculating a Border Score"""
+
+import numpy as np
+from scipy.ndimage import distance_transform_edt
+
+def borderCoverage():
+    '''
+    Calculate border coverage for detected fields.
+    
+    This function calculates firing map border coverage that is further
+    used in calculation of a border score.
+    
+    
+    Parameters
+    ----------
+    fields      :   dict
+        Dictionary of all known firing fields
+        The key is the field_id corresponding to the labelled fields_map
+        Each firing field is also given as a dictionary
+        Example:
+            fields = {1:field_one, 2:field_two}
+            field_one = {'field_id':1, 'field_size':2.6, ...}
+    kwargs
+        search_width    :   int
+            rate_map and fields_map have masked values, which may occur within the region of border 
+            pixels. To mitigate this, we check rows/columns within search_width pixels of the border
+            If no value is supplied, default 8
+        walls           :   str
+            Definition of walls along which the border score is calculated. Provided by
+            a string which contains characters that stand for walls:
+                      T - top wall (we assume the bird-eye view on the arena)
+                      R - right wall
+                      B - bottom wall
+                      L - left wall
+                      Characters are case insensitive. Default value is 'TRBL' meaning that border
+                      score is calculated along all walls. Any combination is possible, e.g.
+                      'R' to calculate along right wall, 'BL' to calculate along two walls, e.t.c.
+    Returns
+    -------
+    coverage    : float
+        Border coverage, ranges from 0 to 1.
+    '''
+    
+    # Extract keyword arguments or set defaults
+    default_search_width = 8
+    default_walls = 'trbl'
+    sw = kwargs.get('search_width', default_search_width)
+    walls = kwargs.get('walls', default_walls).lower()
+    
+    # Check that the wall definition is valid
+    _validate_wall_definition(walls)
+    
+   
+    
+    # Check coverage of each field in turn
+    coverage = 0
+    for field in fields:
+        fmap = field['field map'] # binary image of field: values are 1 inside field, 0 outside
+        if "l" in walls:
+            aux_map = fmap[:,:sw]
+            _wall_field(aux_map)
+        
+        
+        
+        if "r" in walls:
+            pass
+        if "t" in walls:
+            pass
+        if "b" in walls:
+            pass
+    
+    
+def _wall_field(wfmap):
+    '''Evaluate what fraction of the border area is covered by a single field
+    
+    The border area is defined by wfmap - this is the subsection of the binary firing map of a 
+    single field that lies within search_width of a border. wfmap is therefore of size NxM -or- MxN
+    where
+        N = search_width
+        M = arena_size/bin_size
+    Whether NxM or MxN depends on whether looking at left/right or top/bottom walls.
+    
+    
+    
+    '''
+    ly = wfmap.shape[0]
+    inverted_wfmap = 1-wfmap # Want to measure the distance of the wall from the field, 
+                             # while calling distance_transform_edt(wfmap) would give 
+                             # distance of elements of field from non-field
+    
+    distance_transform_edt(inverted_wfmap)
+    
+    ##### TODO TODO TODO
+    # Don't understand this unction in the original MatLab at all
+    # Need to get a datafile to compare with. 
+    
+    
+def _validate_wall_definition(walls):
+    '''Parse the walls argument for invalid entry'''
+    if type(walls) != str:
+        raise ValueException("Wall definition must be given as a string, e.g. 'trbl'. %s is not a valid input." % str(walls))
+    elif len(walls) > 4:
+        raise ValueException("Wall definition may not exceed 4 characters. String '%s' contains %d characters." % (walls, len(walls)))
+    elif len(walls) == 0:
+        raise ValueException("Wall definition must contain at least 1 character from the set [t, r, b, l]")
+    else:
+        for char in walls:
+            if char.lower() not in ["t","r","b","l"]:
+                raise ValueException("Character %s is not a valid entry in wall definition. Valid characters are [t, r, b, l]" % char)
+    
+    
+    
+    
---
+++ opexebo/analysis/rateMapStats.py
@@ -0,0 +1,64 @@
+"""Provide function for calculating statistics of a Rate Map"""
+
+import numpy as np
+
+
+def rateMapStats(rate_map, time_map):
+    '''
+    Calculate statistics of a rate map that depend on probability distribution function (PDF)
+    
+    Calculates information, sparsity and selectivity of a rate map. Calculations are done
+    according to 1993 Skaggs et al. "An Information-Theoretic Approach to Deciphering the Hippocampal Code"
+    paper. Another source of information is 1996 Skaggs et al. paper called
+    "Theta phase precession in hippocampal neuronal populations and the compression of temporal sequences".
+    
+    Parameters
+    ---
+    rate_map        : np.ma.MaskedArray
+        rate map: n x m array where cell value is the firing rate, masked at locations with low occupancy
+    time_map   : np.ma.MaskedArray
+        time map: n x m array where the cell value is the time the animal spent in each cell, masked at locations with low occupancy
+    
+    Returns
+    ---
+    information_rate    : float
+        information rate [bits/sec]
+    information_content : float
+        spatial information content [bits/spike]
+    sparsity            : float
+        see relevant literature (above)
+    selectivity         : float
+        see relevant literature (above)
+     
+    See:
+    ---
+    BNT.+analyses.mapStatsPDF(map)
+    '''
+     
+    
+    duration = np.ma.sum(time_map)
+    position_PDF = time_map / (duration + np.spacing(1)) # Probability distribution of where the animal spent its time. 
+    
+    
+    # Sparsity
+    sparsity = np.nan
+    selectivity = np.nan
+    information_rate = np.nan
+    information_content = np.nan
+    
+    mean_rate = np.ma.sum( rate_map * position_PDF )
+    mean_rate_sq = np.ma.sum( np.ma.power(rate_map, 2) * position_PDF )
+    max_rate = np.ma.max(rate_map)
+    
+    if mean_rate_sq != 0:
+        sparsity = mean_rate * mean_rate / mean_rate_sq
+    
+    if mean_rate != 0:
+        selectivity = max_rate / mean_rate
+        
+        log_argument = np.ma.min(rate_map / mean_rate, 1)
+        information_rate = np.ma.sum(position_PDF * rate_map * np.log2(log_argument))
+        information_content = information_rate / mean_rate
+    
+    
+    return information_rate, information_content, sparsity, selectivity
