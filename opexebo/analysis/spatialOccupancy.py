9ea6b5e85a5a34fbe2747c837e1174fc39549fc9
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2019-05-07 15:30:38 +0200

    Removed smoothing

--- opexebo/analysis/spatialOccupancy.py
+++ opexebo/analysis/spatialOccupancy.py
@@ -2,9 +2,9 @@
 Provides function to calculate the spatial occupancy of the arena
 '''
 import numpy as np
-from scipy.ndimage.filters import gaussian_filter
+from opexebo.general import validatekeyword__arena_size
 
-def spatialOccupancy(positions, speed, **kwargs):
+def spatialoccupancy(positions, speed, **kwargs):
     '''
     Generate an occpuancy map: how much time the animal spent in each location
     in the arena.    
@@ -35,8 +35,7 @@ def spatialOccupancy(positions, speed, **kwargs):
         speed_cutoff    : float. 
             Timestamps with instantaneous speed exceeding this value are ignored. 
             Default infinity
-        sigma           : float.  
-            Parameter for gaussian smoothing, in units of bins. Default 1
+
     '''
     # Check for correct shapes. 
     dimensionality, num_samples = positions.shape
@@ -52,28 +51,17 @@ def spatialOccupancy(positions, speed, **kwargs):
     default_bin_width = 2.5
     default_arena_size = 80
     default_speed_cutoff = np.inf
-    default_sigma = 1
     
     bin_width = kwargs.get("bin_width", default_bin_width)
     arena_size = kwargs.get("arena_size", default_arena_size)
     speed_cutoff = kwargs.get("speed_cutoff", default_speed_cutoff)
-    sigma = kwargs.get("sigma", default_sigma)
     
     # Handle the distinctions with arena sizes: 1d or 2d, different kinds of 2d
     # If a single arena dimensions is provided, it treats the arena as either circular/square (len x len)
     # or linear (len x 1).. In both cases, num_bins is an int
     # If two arena dimensions are provided, it treats it as a rectangle (len1 x len2)
     # in this case, num_bins is an array.
-    is_2d = bool(dimensionality - 2) # i.e. if positions has 2 columns [t,x], this will evaluate to False
-    if type(arena_size) in (list, tuple, np.ndarray):
-        if len(arena_size) == 1:
-            arena_size = float(arena_size)            
-        elif len(arena_size) > 2:
-            raise ValueError("Keyword 'arena_size' value is invalid. Provide either a float or a 2-element tuple")
-        elif len(arena_size) == 2 and not is_2d:
-            raise ValueError("Mismatch in dimensions: 1d position data but 2d arena specified")
-        else:
-            arena_size = np.array(arena_size)
+    arena_size, is_2d = validatekeyword__arena_size(arena_size, dimensionality-1)
     num_bins = np.ceil(arena_size / bin_width)
     
     # Calculate occupancy
@@ -102,12 +90,10 @@ def spatialOccupancy(positions, speed, **kwargs):
     frame_duration = np.min(np.diff(time_stamps))    
     occupancy_map_time = occupancy_map / frame_duration
     
-    # Apply smoothing
-    occupancy_map_time = gaussian_filter(occupancy_map_time, sigma=sigma, mode='nearest')
-    masked_map = np.masked_where(occupancy_map == 0, occupancy_map_time)
+
+    masked_map = np.ma.masked_where(occupancy_map == 0, occupancy_map_time)
     
     return masked_map
     
     
      
-    
