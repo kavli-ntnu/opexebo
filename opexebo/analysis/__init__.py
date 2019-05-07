9aef7fe8a26f708a35920baba49d0d8ed9c9070d
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2019-05-07 16:38:26 +0200

    Updated imports

--- opexebo/analysis/__init__.py
+++ opexebo/analysis/__init__.py
@@ -1,5 +1,13 @@
 from opexebo.analysis.gridscore import gridscore
 from opexebo.analysis.autocorrelation import autocorrelation
 from opexebo.analysis.placefield import placefield
+from opexebo.analysis.angularoccupancy import angularoccupancy
+from opexebo.analysis.bordercoverage import bordercoverage
+from opexebo.analysis.borderscore import borderscore
+from opexebo.analysis.ratemap import ratemap
+from opexebo.analysis.ratemapstats import ratemapstats
+from opexebo.analysis.spatialoccupancy import spatialoccupancy
 
-__all__ = ["gridscore", "autocorrelation", "placefield"]
+__all__ = ["gridscore", "autocorrelation", "placefield", "angularoccupancy", 
+           "bordercoverage", "borderscore", "ratemap", "ratemapstats", 
+           "spatialoccupancy"]
--- opexebo/general/__init__.py
+++ opexebo/general/__init__.py
@@ -1,4 +1,8 @@
 from opexebo.general.normxcorr2_general import normxcorr2_general
+from opexebo.general.smooth import smooth
 
-__all__ = ['normxcorr2_general',
+from opexebo.general.validatekeyword__arena_size import validatekeyword__arena_size
+
+__all__ = ['normxcorr2_general', 'smooth', 
+           "validatekeyword__arena_size"
 ]
