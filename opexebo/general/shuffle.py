e961a1db4378b4eceec6efe4bcdde3e43cfc4d9f
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2019-05-15 11:02:57 +0200

    Correct shuffling method

--- opexebo/general/shuffle.py
+++ opexebo/general/shuffle.py
@@ -1,8 +1,9 @@
 """Provide function for shuffling"""
 
 import numpy as np
+import time
 
-def shuffle(times, t_min, t_max, iterations):
+def shuffle(times, t_min, t_max, iterations, **kwargs):
     '''
     Increment the provided time series by a random period of time in order to 
     destroy the correlation between spike times and animal behaviour.
@@ -11,8 +12,16 @@ def shuffle(times, t_min, t_max, iterations):
         * initially, we have two time series, Tracking and SpikeTimes, both with 
         values in the range [t0, t1]. 
         * after incrementing by T, we have Tracking in [t0, t1] and SpikeTimes
-        in [t0+T, T1+T]
+        in [t0+T, t1+T]
         * Take all spike times in the range [t1, t1+T] and map back to [t0, t0+T]
+        by subtracting (t1-t0)
+    
+    The end result should be a series of times also in the range [t0, t1], 
+    with the same intervals between times**, but the exact value of those times
+    has changed. 
+    
+    ** with 1 exception, at the gap between where the oldest times end and
+    the first times begin
     
     The random numbers are drawn from a pseudorandom, uniform, distribution in 
     the range [t_min, t_max], seeded 
@@ -28,6 +37,18 @@ def shuffle(times, t_min, t_max, iterations):
         the maximum value that the time series can be incremented by
     iterations : int
         How many copies of times should be returned (each copy incremented by a random offset)
+    kwargs
+        'debug' : bool
+            Enable additional debugging output
+        'tracking_range' : array_like
+            The time range over which tracking behaviour exists, defining the
+            times at which spike indexes are looped back on themselves
+            This can be provided either as a 2-element tuple (t0, t1), or as 
+            the entire list of tracking timestamps. In each case, the min and 
+            max values are used as t0, t1
+            If no values are provided, then the first and last spike times
+            are used. This is not desirable behaviour, since it will then guarantee 
+            that in the shuffled output, two spikes will occur simultaneously
     
     Returns
     -------
@@ -36,7 +57,7 @@ def shuffle(times, t_min, t_max, iterations):
     
     See also
     --------
-    BNT.+scripts.shuffling
+    BNT.+scripts.shuffling (around line 350)
     '''
     
     # Check values
@@ -45,6 +66,16 @@ def shuffle(times, t_min, t_max, iterations):
     if times.ndim != 1:
         raise ValueError("You must provide a 1D array of times. You provided a %d-dimensional array" % times.ndim)
     
+    debug = kwargs.get('debug', False)
+    tr = kwargs.get('tracking_range', None)
+    if tr:
+        t0 = np.min(tr)
+        t1 = np.max(tr)
+    else:
+        t0 = np.min(times)
+        t1 = np.max(times)
+
+    
     # Initialise Numpy's random number generator with a new seed based on the 
     # user's local computer OS methods
     
@@ -52,10 +83,13 @@ def shuffle(times, t_min, t_max, iterations):
     increments = np.random.RandomState().rand(iterations) # Uniformmly distrbuted in [0, 1]
     increments = t_min + (increments * (t_max-t_min))
     
-    t0 = np.min(times)
-    t1 = np.max(times)
-    T = np.max(increments)
     num_spikes = np.size(times)
+    
+    if debug:
+        print("Number of spikes provided: %d" % num_spikes)
+        print("Spikes in time range [%d, %d]" % (t0, t1))
+        print("Iterations requested: %d" % iterations)
+        print("Increments in range [%.2f, %.2f]" % (np.min(increments), np.max(increments)))
     
     
     # Generate 2D arrays by repeating over the correct axis
@@ -65,27 +99,52 @@ def shuffle(times, t_min, t_max, iterations):
     all_inc = np.repeat(increments[:, np.newaxis], times.size, axis=1).transpose()
     new_times = all_times + all_inc
     
-    to_shift = (new_times > (t1-increments)) # Binary array of the elements that need to be moved to the beginning. Each column is different
+    
+    
+    to_shift = (new_times > t1) # Binary array of the elements that need to be moved to the beginning. Each column is different
     time_idx = num_spikes - np.count_nonzero(to_shift, axis=0) # indicies of the first time in each iteration which must be moved
-    iter_idx = np.arange(iterations)
+    if debug:
+        print("New time range: [%d, %d]" % (np.min(new_times), np.max(new_times)))
+        print("Indexes exceeding value t1: %s" % to_shift)
+        
     output = np.zeros(new_times.shape)
 
-    for i in iter_idx:
+    a0 = time.time()
+    for i in range(iterations):
         # Circular buffer. We have alread identified the index at which elements 
         # need to be moved (time_idx[i]). So take those end elements, and move 
         # them to the start
         # Turns out, this is actually faster than fancy array indexing with np.ix_(time_idx, iter_idx), by nearly 100x (!) in testing
-        output[0:num_spikes-time_idx[i], i] = new_times[time_idx[i]:, i] - increments[i]
-        output[time_idx[i]:, i] = new_times[:num_spikes-time_idx[i], i]
+        
+        # Elements that are still within the time range[t0+T, t1]
+        a = new_times[0:time_idx[i], i]
+        
+        # Elements that are in the time range [t1, t1+T]
+        # len(b) == num_spikes-time_idx[i]
+        b = new_times[time_idx[i]:, i]
+        
+        # Elements in b should have their values reduced to fit in the range [t0, t0+T]        
+        b = b - t1 + t0
+        
+        # Insert elements into output in the correct order: b first, then a filling in the blanks after
+        output[0:num_spikes-time_idx[i], i] = b
+        output[num_spikes-time_idx[i]:, i] = a
 
+    a1 = time.time()
+    if debug:
+        print("Final time range: [%d, %d]" % (np.min(output), np.max(output)))
+        print("method 1 took %dms" % ((a1-a0)*1000))
+        print(np.diff(times))
+        print(np.diff(output[:,0]))
     
     return output
     
 if __name__ == '__main__':
-    times = np.arange(5000)
-    t_min = 17
-    t_max = 42
-    iterations = 200
-    s = shuffle(times, t_min, t_max, iterations)
+    times = [2, 12, 27, 54, 82, 113, 115, 207, 300]
+    t_min = 20
+    t_max = max(times)-t_min
+    iterations = 1
+    s = shuffle(times, t_min, t_max, iterations, debug=1, tracking_range=(0,349))
+    #print(s)
 
     
