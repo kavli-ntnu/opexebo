c5cfa35009eec55ec94553b6c08a52fbf062d205
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2019-05-20 10:55:20 +0200

    Update __init__.py

--- opexebo/analysis/__init__.py
+++ opexebo/analysis/__init__.py
@@ -7,7 +7,8 @@ from opexebo.analysis.borderscore import borderscore
 from opexebo.analysis.ratemap import ratemap
 from opexebo.analysis.ratemapstats import ratemapstats
 from opexebo.analysis.spatialoccupancy import spatialoccupancy
+from opexebo.analysis.speedscore import speedscore
 
 __all__ = ["gridscore", "autocorrelation", "placefield", "angularoccupancy", 
            "bordercoverage", "borderscore", "ratemap", "ratemapstats", 
-           "spatialoccupancy"]
+           "spatialoccupancy", "speedscore"]
