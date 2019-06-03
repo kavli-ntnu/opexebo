b3ef08f0172bec6e372c319cb448eecc635a696b
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2019-06-03 14:34:29 +0200

    Discount non-visited cells for euler number

--- opexebo/analysis/placefield.py
+++ opexebo/analysis/placefield.py
@@ -5,7 +5,7 @@ Provide function for 2D placefield detection.
 import numpy as np
 
 from scipy import ndimage
-from skimage import measure, morphology
+from skimage import measure, morphology             # scikit-image package
 import opexebo.defaults as default
 
 
@@ -18,10 +18,18 @@ def placefield(firing_map, **kwargs):
     area doesn't change any more or the area explodes (this means the threshold 
     is too low).
     
+    General approach:
+        1 : Handle inputs
+        2 : Identify local maxima
+        3 : Identify fields
+        4 : filter fields by criteria
+    
     Parameters
     ----------
     firing_map : np.ndarray or np.ma.MaskedArray
-        smoothed rate map
+        smoothed rate map. 
+        If supplied as an np.ndarray, it is assumed that the map takes values of np.nan at locations of zero occupancy
+        If supplied as an np.ma.MaskedArray, it is assumed that the map is masked at locations of zero occupancy
     **kwargs
         min_bins : int
             Fields containing fewer than this many bins will be discarded.
@@ -64,6 +72,7 @@ def placefield(firing_map, **kwargs):
     (at your option) any later version.
     
     """
+    ''' Part 1: handle inputs'''
     # Get keyword arguments
     min_bins = kwargs.get("min_bins", default.firing_field_min_bins)
     min_peak = kwargs.get("min_peak", default.firing_field_min_peak)
@@ -74,14 +83,20 @@ def placefield(firing_map, **kwargs):
     global_peak = np.nanmax(firing_map)
     if np.isnan(global_peak) or global_peak == 0:
         return None
-
-    nan_indices = np.isnan(firing_map)
-    firing_map = np.nan_to_num(firing_map)
     
-        
+    # Get details of where the animal spent zero time, then discard NaNs (if any)
+    # This is a binary image - False if the animal visited that bin,
+    # True if the animal did NOT visit that bin
+    if type(firing_map) == np.ma.MaskedArray:
+        occupancy_mask = firing_map.mask
+    else:
+        occupancy_mask = np.isnan(firing_map)
+    firing_map = np.nan_to_num(firing_map, copy=True)
+    
+    
+    '''Part 2: find local maxima'''        
     # disc structural element of size 1
     se = morphology.disk(1)
-
     Ie = morphology.erosion(firing_map, se)
     Iobr = morphology.reconstruction(Ie, firing_map)
     # The effect of this erosion/reconstruction is to reduce value of cells 
@@ -89,8 +104,8 @@ def placefield(firing_map, **kwargs):
     # plt.imshow(np.ma.masked_where(firing_map-Iobr!=0, firing_map))
     # Also well explained here 
     # https://se.mathworks.com/help/images/understanding-morphological-reconstruction.html
-    # This acts to discard some minor local maxima, especially in poorly connected
-    # parts of the ratemap
+    # Not 100% clear on the exact purpose -Simon
+    # However, it *seems* as if it may act to suppress very small local maxima
 
 
     # this is regionmax equivalent
@@ -122,19 +137,21 @@ def placefield(firing_map, **kwargs):
     good_peaks = (found_peaks >= min_peak)
     peak_coords = peak_coords[good_peaks, :]
 
-    I = Iobr
-    max_value = np.max(I)
+
+    '''Part 3: From local maxima, get fields by expanding around maxima'''
+    max_value = np.max(Iobr)
     # prevent peaks with small values from being detected
     # SWB - This causes problems where a local peak is next to a cell that the animal never went
     # As that risks the field becoming the entire null region
     # Therefore, adding 2nd criterion to avoid adding information where none was actually known. 
-    I[np.logical_and(I < min_peak, I > 0.01)] = max_value * 1.5
+    Iobr[np.logical_and(Iobr < min_peak, Iobr > 0.01)] = max_value * 1.5
 
     # this can be confusing, but this variable is just an index for the vector
     # peak_linear_ind
     peaks_index = np.arange(len(peak_coords))
 
-    fields_map = np.zeros(I.shape, dtype=np.integer)
+
+    fields_map = np.zeros(Iobr.shape, dtype=np.integer)
     field_id = 1
 
     for i, peak_rc in enumerate(peak_coords):
@@ -145,50 +162,61 @@ def placefield(firing_map, **kwargs):
         if other_fields.size > 0:
             other_fields_linear = np.ravel_multi_index(
                     multi_index=(other_fields[:, 0], other_fields[:, 1]),
-                    dims=I.shape, order='F')
+                    dims=Iobr.shape, order='F')
         else:
             other_fields_linear = []
 
         used_th = 0.96
-        res = _area_change(I, peak_rc, 0.96, 0.94, other_fields_linear)
+        res = _area_change(Iobr, occupancy_mask, peak_rc, 0.96, 0.94, other_fields_linear)
         initial_change = res['acceleration']
         area2 = res['area2']
         first_pixels = np.nan
         if np.isnan(initial_change):
             for j in np.linspace(0.97, 1., 4):
                 # Thresholds get higher, area should tend downwards to 1 (i.e. only including the actual peak)
-                res = _area_change(I, peak_rc, j, j-0.01, other_fields_linear)
+                res = _area_change(Iobr, occupancy_mask, peak_rc, j, j-0.01, other_fields_linear)
                 initial_change = res['acceleration']
                 area1 = res['area1']
                 area2 = res['area2']
+                # initial_change is the change from area1 to area 2
+                # area2>area1 -> initial_change > 1
+                # area2<area1 -> initial_change < 1
+                # area 2 is calculated with lower threshold - should usually be larger
                 first_pixels = res['first_pixels']
                 if not np.isnan(initial_change) and initial_change > 0:
+                    # True is both area1 and area2 are valid
+                    # Weird conditonal from Vadim - initial change will EITHER:
+                    # be greater than zero (can't get a negative area to give negative % change)
+                    # OR be NaN (which will always yield false when compared to a number)
                     used_th = j - 0.01
                     break
 
             if np.isnan(initial_change) and not np.isnan(area1):
                 # For the final change
-                pixels = np.unravel_index(first_pixels, I.shape, 'F')
-                I[pixels] = max_value * 1.5
+                pixels = np.unravel_index(first_pixels, Iobr.shape, 'F')
+                Iobr[pixels] = max_value * 1.5
                 fields_map[pixels] = field_id
                 field_id = field_id + 1
-                continue
 
             if np.isnan(initial_change):
                 # failed to extract the field
-                continue
+                # Do nothing and continue for-loop
+                pass
 
-        pixel_list = _expand_field(I, peak_rc, initial_change, area2,
+        pixel_list = _expand_field(Iobr, occupancy_mask, peak_rc, initial_change, area2,
                                    other_fields_linear, used_th)
         if np.any(np.isnan(pixel_list)):
             # nu - not used
-            nu, pixel_list, nu = _area_for_threshold(I, peak_rc, used_th+0.01, other_fields_linear)
-
-        pixels = np.unravel_index(pixel_list, I.shape, 'F')
-        I[pixels] = max_value * 1.5
+            nu, pixel_list, nu = _area_for_threshold(Iobr, occupancy_mask, peak_rc, used_th+0.01, other_fields_linear)
+        if len(pixel_list)>0:
+            pixels = np.unravel_index(pixel_list, Iobr.shape, 'F')
+            
+        Iobr[pixels] = max_value * 1.5
         fields_map[pixels] = field_id
         field_id = field_id + 1
-
+    
+    
+    '''Part 4: Determine which, if any, fields meet filtering criteria'''
     regions = measure.regionprops(fields_map)
 
     fields = []
@@ -228,34 +256,49 @@ def placefield(firing_map, **kwargs):
                 print("Field size too small (%d)" % num_bins)
             if mean_rate < min_mean:
                 print("Field mean rate too low (%.2f Hz)" % mean_rate)
-        
+        else:
+            # Field too small and debugging information not needed
+            # Do nothing
+            pass
 
     return (fields, fields_map)
 
 
-def _expand_field(I, peak_rc, initial_change, initial_area, other_fields_linear, initial_th):
+def _expand_field(I, occupancy_mask, peak_rc, initial_change, initial_area, other_fields_linear, initial_th):
+    '''
+    Adaptive placefield detection:
+        Start with a threshold around 80%, step down in ~0.02
+        Measure field at threshold
+        If the field is invalid, take the previous iteration
+        If field size has decreased, keep count
+        If field size has increased by less than 300% of initial_change AND the field has decreased in size fewer than 3 times in a row
+            If the field size hasn't changed in 10 steps, return the current field size
+        Else
+        
+        
+    '''
     pixel_list = np.nan
     last_area = initial_area
     last_pixels = []
     num_not_changing = 0
     num_decrease = 0
 
-    num_steps = np.round((initial_th - 0.2) / 0.02) + 1
-    for i in np.linspace(initial_th, 0.2, num_steps):
-        i = round(i, 2)
+    num_steps = int((initial_th - 0.2) / 0.02) + 1
+    for threshold in np.linspace(initial_th, 0.2, num_steps):
+        threshold = np.round(threshold, 2)
 
-        area, pixels, is_bad = _area_for_threshold(I, peak_rc, i, other_fields_linear)
+        area, pixels, is_bad = _area_for_threshold(I, occupancy_mask, peak_rc, threshold, other_fields_linear)
         if np.isnan(area) or is_bad:
             pixel_list = last_pixels
             break
-        current_change = area / last_area * 100
-        if current_change < 100:
+        current_change = area / last_area
+        if current_change < 1:
             num_decrease = num_decrease + 1
         else:
             num_decrease = 0
 
         if np.floor(current_change / initial_change) <= 2 and num_decrease  < 3:
-            if current_change == 100:
+            if current_change == 1:
                 num_not_changing = num_not_changing + 1
             else:
                 num_not_changing = 0
@@ -278,7 +321,7 @@ def _expand_field(I, peak_rc, initial_change, initial_area, other_fields_linear,
     return pixel_list
 
 
-def _area_change(I, peak_rc, first, second, other_fields_linear):
+def _area_change(I, occupancy_mask, peak_rc, first, second, other_fields_linear):
     ''' Compare the change in field area based on two threshold values
     
     If either threshold results in an invalid field (based on criteria in 
@@ -290,6 +333,7 @@ def _area_change(I, peak_rc, first, second, other_fields_linear):
     Params
     ------
     I : np.ndarray                      : Image
+    occupancy_mask: np.ndarray          : binary map - True where the animal spent zero time
     peak_rc : tuple                     : [row, col] of local maxima
     first : float                       : first threshold
     second : float                      : second threshold
@@ -299,17 +343,17 @@ def _area_change(I, peak_rc, first, second, other_fields_linear):
                'first_pixels': np.nan,
                'second_pixels': np.nan}
 
-    area1, first_pixels, is_bad1 = _area_for_threshold(I, peak_rc, first,
+    area1, first_pixels, is_bad1 = _area_for_threshold(I, occupancy_mask, peak_rc, first,
                                                        other_fields_linear)
     if np.isnan(area1) or is_bad1:
         return results
 
-    area2, second_pixels, is_bad2 = _area_for_threshold(I, peak_rc, second,
+    area2, second_pixels, is_bad2 = _area_for_threshold(I, occupancy_mask, peak_rc, second,
                                                         other_fields_linear)
     if np.isnan(area2) or is_bad2:
         return results
 
-    acceleration = area2 / area1 * 100
+    acceleration = area2 / area1 #* 100
     results['acceleration'] = acceleration
     results['area1'] = area1
     results['area2'] = area2
@@ -319,7 +363,7 @@ def _area_change(I, peak_rc, first, second, other_fields_linear):
     return results
 
 
-def _area_for_threshold(I, peak_rc, th, other_fields_linear):
+def _area_for_threshold(I, occupancy_mask, peak_rc, th, other_fields_linear):
     '''Calculate the area of the field defined by the local maxima 'peak_rc' and
     the relative thresholding value 'th'
     
@@ -342,7 +386,7 @@ def _area_for_threshold(I, peak_rc, th, other_fields_linear):
     area_linear_indicies : np.ndarray
         indicies of all cells within field if field is valid
     is_bad : bool
-        True IF field includes a second local maxima
+        True IF field includes a second local maxima or IF field contains holes
     '''
     ar = np.nan
     # Field is bad if it contains any other peak
@@ -370,13 +414,15 @@ def _area_for_threshold(I, peak_rc, th, other_fields_linear):
     # This yields results that are more similar to Matlab's regionprops
     # NOTE - this uses scipy.ndimage.morphology, while most else uses skimage.morphology
     filled_image = ndimage.morphology.binary_fill_holes(labeled_img)
-    euler_array = (filled_image != labeled_img)  # The cells that were filled in
+    euler_array = (filled_image != labeled_img)  # True where holes were filled in
+    #euler_array = np.maximum((euler_array*1) - (occupancy_mask*1), 0)
     euler_objects = morphology.label(euler_array, connectivity=2) # connectivity=2 : vertical, horizontal, and diagonal
     num = np.max(euler_objects) # How many holes were filled in
     euler_number = -num + 1
 
     if euler_number <= 0:
         # If any holes existed, then return this
+        is_bad = True
         return (ar, [], is_bad)
 
     regions = measure.regionprops(labeled_img)
