91c5b0dfa61f2784880bd8f8f69434fd891293bb
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2019-06-04 14:40:19 +0200

    Added Ellipse Fitting
    Based on Vadim's Matlab implementation. Produces values accurate to the precision displayed by MatLab (5sf)

--- opexebo/general/__init__.py
+++ opexebo/general/__init__.py
@@ -1,7 +1,8 @@
 from opexebo.general.normxcorr2_general import normxcorr2_general
 from opexebo.general.smooth import smooth
+from opexebo.general.fitellipse import fitellipse
 from opexebo.general.validatekeyword import validatekeyword__arena_size
 from opexebo.general.accumulatespatial import accumulatespatial
 
 __all__ = ['normxcorr2_general', 'smooth', 'accumulatespatial',
-           'validatekeyword__arena_size']
+           'validatekeyword__arena_size', 'fitellipse']
