54f3fe2e4df58dd78d1da12df69e4f2a2073b9e8
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2019-09-04 15:47:20 +0200

    Bumpversion to 0.3.1

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     name='opexebo',
     packages=find_packages(include=['opexebo*']),
     url='https://github.com/kavli-ntnu/opexebo',
-    version='0.3.0',
+    version='0.3.1',
     zip_safe=False,
 )
