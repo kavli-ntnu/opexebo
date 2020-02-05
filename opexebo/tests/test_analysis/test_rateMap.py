2f63a2e5d10f05bfa7491282ceaa4d7bbf262616
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2020-02-05 11:39:25 +0100

    Update test_rateMap.py

--- opexebo/tests/test_analysis/test_rateMap.py
+++ opexebo/tests/test_analysis/test_rateMap.py
@@ -11,52 +11,34 @@ import numpy as np
 import pytest
 
 import test_helpers as th
+import opexebo
 from opexebo.analysis import rate_map as func
 
 print("=== tests_analysis_rateMap ===")
 
-###############################################################################
-################                HELPER FUNCTIONS
-###############################################################################
 
-
-
-def get_ratemap_opexebo(data, key, arena_size = 80, bin_width=2.0):
-    if type(arena_size)==int:
-        lim = (-arena_size/2, arena_size/2, -arena_size/2, arena_size/2)
-    elif type(arena_size) in (tuple, list, np.ndarray):
-        lim = (-arena_size[0]/2, arena_size[0]/2, -arena_size[1]/2, arena_size[1]/2)
-
-    time_map = data['cellsData'][key,0]['epochs'][0,0][0,0]['map'][0,0]['timeRaw'][0,0]
-    spikes = data['cellsData'][key,0]['epochs'][0,0][0,0]['spikes2Pos'][0,0].transpose()
-    rmap_raw = func(time_map, spikes, arena_size=arena_size, 
-                                           bin_width=bin_width, limits=lim)
-    return rmap_raw
-
-
-def get_ratemap_bnt(data, key):
-    rmap_smooth = data['cellsData'][key,0]['epochs'][0,0][0,0]['map'][0,0]['z'][0,0]
-    rmap_raw = data['cellsData'][key,0]['epochs'][0,0][0,0]['map'][0,0]['zRaw'][0,0]
-    return rmap_smooth, rmap_raw
-
-def rms(img):
-    return np.sqrt(np.nanmean(np.square(img)))
 
 ###############################################################################
 ################                MAIN TESTS
 ###############################################################################
     
 def test_rmap_invalid_inputs():
-    # Mismatched dimensions 
+    # Mismatched dimensions
+    # 2d time map but 1d spike pos
     with pytest.raises(ValueError):
-        tmap = np.ones((10, 10)) #x, y
-        spikes = np.ones((2, 100)) # t, x
-        func(tmap, spikes, arena_size=1)
-    with pytest.raises(ValueError):
-        tmap = np.ones(10) #x
-        spikes = np.ones((3, 100)) # t, x, y
-        func(tmap, spikes, arena_size = 1)
-    
+        tmap = np.ones((10, 10))
+        time = np.arange(100)
+        spikes_x = np.random.rand(100)
+        spikes_tracking = np.array((time, spikes_x))
+        func(tmap, spikes_tracking, arena_size=1)
+    #1d time map but 2d spike_pos
+    with pytest.raises(IndexError):
+        tmap = np.ones(10)
+        time = np.arange(100)
+        spikes_x = np.random.rand(100)
+        spikes_y = np.random.rand(100)
+        spikes_tracking = np.array((time, spikes_x, spikes_y))
+        func(tmap, spikes_tracking, arena_size=(10, 10))
     # invalid input types
     with pytest.raises(ValueError):
         tmap = (1,2,3,4) #! tmap should be an ndarray
@@ -66,15 +48,14 @@ def test_rmap_invalid_inputs():
         tmap = np.ones(10)
         spikes = ([0.23,1], [0.5, 1.2], [0.75, -3]) #! spikes should be an ndarray
         func(tmap, spikes, arena_size=1)
-    with pytest.raises(KeyError):
+    with pytest.raises(ValueError):
         tmap = np.ones((10,10))
         spikes = np.ones((3, 100)) # t, x, y
         func(tmap, spikes) #! missing arena_size
     with pytest.raises(ValueError):
         tmap = np.ones((10,10))
         spikes = np.ones((3, 100))
-        func(tmap, spikes, arena_size=1, limits="abc") # limits should be a tuble
-    
+        func(tmap, spikes, arena_size=1, limits="abc") # limits should be a tuple
     # mismatched sizes
     with pytest.raises(ValueError):
         tmap = np.ones((40,40))
@@ -85,37 +66,42 @@ def test_rmap_invalid_inputs():
     return True
     
     
-def test_rmap_simple():
-    data = spio.loadmat(th.test_data_square)
-    ds = np.arange(th.get_data_size(data))
+def test_1d_ratemap():
     arena_size = 80
-    bin_width = 2.0
-    rtol = 1e-3
+    bin_width = 2.5
+    _num_bins = opexebo.general.bin_width_to_bin_number(arena_size, bin_width)
+    tmap = np.ones(_num_bins) # equal occupancy in all locations
+    n = 5000
+    times = np.sort(np.random.rand(n)) * 1200 # 20 minute session
+    speeds = np.random.rand(n) * 10 # linearly distributed speeds up to 10cm/s
+    pos = np.random.rand(n) * arena_size
+    spikes_tracking = np.array([times, speeds, pos])
+    speed_cutoff = 2
+    rmap = func(tmap, spikes_tracking, bin_width=bin_width, speed_cutoff=speed_cutoff, arena_size=arena_size)
+    assert rmap.ndim == 1
+    assert rmap.shape == tmap.shape
+
+def test_2d_ratemap():
+    arena_size = (80, 120)
+    bin_number = (16, 24)
+    tmap = np.ones(bin_number).T
+    n = 5000
+    times = np.sort(np.random.rand(n)) * 1200 # 20 minute session
+    speeds = np.random.rand(n) * 10 # linearly distributed speeds up to 10cm/s
+    pos_x = np.random.rand(n) * arena_size[0]
+    pos_y = np.random.rand(n) * arena_size[1]
+    spikes_tracking = np.array([times, speeds, pos_x, pos_y])
+    speed_cutoff = 2
+    rmap = func(tmap, spikes_tracking, bin_number=bin_number, speed_cutoff=speed_cutoff, arena_size=arena_size)
+    assert rmap.ndim == 2
+    assert rmap.shape == tmap.shape
     
-    for key in ds:
     
-        bnt = get_ratemap_bnt(data, key)[1] # raw map only
-        ope = get_ratemap_opexebo(data, key, arena_size=arena_size, bin_width=bin_width)
-
-        #  Test that the binning and arena size is valid
-        assert(bnt.shape == ope.shape)
-        
-        # Test that the voids in the map due to the animal not visiting are 
-        # consistent
-        assert(np.isnan(bnt).all() == ope.mask.all())
-        
-        # Test that the map produces similar absolute rates
-        assert(np.isclose(np.nanmean(bnt), np.nanmean(ope), rtol = rtol))
-        
-        # Test that the maximum and minimum values are similar
-        assert(np.isclose(np.nanmax(bnt), np.nanmax(ope), rtol = rtol))
-        assert(np.isclose(np.nanmin(bnt), np.nanmin(ope), rtol = rtol))
-        
-    print("test_ratemap_simple passed")
-    return True
+    
 
 
 
-if __name__ == '__main__':
-    test_rmap_invalid_inputs()
-    test_rmap_simple()
+#if __name__ == '__main__':
+#    test_rmap_invalid_inputs()
+#    test_1d_ratemap()
+#    test_2d_ratemap()
