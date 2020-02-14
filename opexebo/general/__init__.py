b48d04fd4da88472d3d58e9a87e94e95051cfac5
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2020-02-14 13:31:26 +0100

    Upsampling added

--- opexebo/general/__init__.py
+++ opexebo/general/__init__.py
@@ -2,6 +2,7 @@ from opexebo.general.normxcorr2_general import normxcorr2_general
 from opexebo.general.smooth import smooth
 from opexebo.general.shuffle import shuffle
 from opexebo.general.fitEllipse import fit_ellipse
+from opexebo.general.upsample import upsample
 from opexebo.general.validateKeyword import validatekeyword__arena_size
 from opexebo.general.binWidth_to_binNumber import bin_width_to_bin_number
 from opexebo.general.circular_mask import circular_mask
@@ -14,4 +15,4 @@ from opexebo.general.spatialCrossCorrelation import spatial_cross_correlation
 __all__ = ['normxcorr2_general', 'smooth', 'accumulate_spatial', 'shuffle',
            'validatekeyword__arena_size', 'fit_ellipse', 'peak_search',
            'power_spectrum', 'spatial_cross_correlation', 'bin_width_to_bin_number',
-           'circular_mask']
+           'circular_mask', 'upsample']
---
+++ opexebo/tests/test_general/test_upsample.py
@@ -0,0 +1,86 @@
+""" Tests for upsampling"""
+import os
+os.environ['HOMESHARE'] = r'C:\temp\astropy'
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+import numpy as np
+import pytest
+
+from opexebo.general import upsample as func
+
+print("=== tests_upsampling ===")
+
+
+
+
+###############################################################################
+################                MAIN TESTS
+###############################################################################
+
+
+
+def test_invalid_inputs():
+    # Invalid array format
+    with pytest.raises(NotImplementedError):
+        upscale = 2
+        func(3, upscale)
+        func("abc", upscale)
+        func({2:2}, upscale)
+    
+    # Invalid integerr upscaling
+    # Force integer upsclaing with masked array inputs
+    with pytest.raises(NotImplementedError):
+        masked_array = np.ma.ones((4,6))
+        high_dim_ma = np.ma.ones((4,6,7))
+        fractional_upscale = 2.5
+        shrinking_upscale = 0
+        valid_upscale = 2
+        func(masked_array, fractional_upscale)
+        func(masked_array, shrinking_upscale)
+        func(high_dim_ma, valid_upscale)
+    
+    # Invalid fractional upscaling
+    # force fractional upscaling with floating point upscale
+    with pytest.raises(ValueError):
+        ndarray = np.ones((4,6))
+        zero_upscale = float(0)
+        neg_upscale = -0.5
+        func(ndarray, zero_upscale)
+        func(ndarray, neg_upscale)
+
+    print("test_invalid_inputs passed")
+    return True
+
+def test_integer_scaling_2d():
+    upscale = int(2)
+    ndarray = np.random.rand(50,50)
+    maarray = ndarray.copy()
+    maarray[25:28, 10:16] = np.nan
+    maarray = np.ma.masked_invalid(maarray)
+    
+    # Test behavior on masked array
+    new_array = func(maarray, upscale)
+    assert np.array_equal(np.array(maarray.shape) * upscale,
+                          np.array(new_array.shape))
+    assert np.sum(maarray.mask) * (upscale**2) == np.sum(new_array.mask)
+    
+    # Test behavior on ndarray
+    new_array = func(ndarray, upscale)
+    assert np.array_equal(np.array(ndarray.shape) * upscale,
+                          np.array(new_array.shape))
+    print("test_integer_scaling_2d passed")
+    return True
+
+def test_fractional_scaling_2d():
+    upscale = float(1.45)
+    ndarray = np.random.rand(50,75)
+    new_array = func(ndarray, upscale)
+    assert np.array_equal(np.round(np.array(ndarray.shape) * upscale), new_array.shape)
+    print("test_fractional_scaling_2d passed")
+    return True
+
+#if __name__ == '__main__':
+#    test_invalid_inputs()
+#    test_integer_scaling_2d()
+#    test_fractional_scaling_2d()
