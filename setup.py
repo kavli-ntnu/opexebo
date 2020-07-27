b8608e8a88cfc6bffc4e8158a2a7c6c3ff8f2a4d
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2020-07-27 14:40:47 +0200

    Update Python3 compatibility
    Use of f-strings mean that the library is not compatible with versions earlier than 3.6

--- setup.py
+++ setup.py
@@ -30,11 +30,10 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3'
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="Collection of python code in Kavli lab.",
     install_requires=requirements,
