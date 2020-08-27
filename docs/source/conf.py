c5719f6974e28fa2d5682833ac90a9d021574f6f
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2020-08-27 13:21:48 +0200

    Add readthedocs integration
    * fix conf.py breaking sphinx
    * fix setup.py brteaking conf.py on bumpversion
    * add readthedocs config
    * add readthedocs badge
    * switch from rst to md readme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,9 @@ copyright = '2019, Simon Ball'
 author = 'Simon Ball'
 
 # The short X.Y version
-version = 'version = 'version = '0.5.0'''
+version = '0.5.0'
 # The full version, including alpha/beta/rc tags
-release = 'version = 'version = '0.5.0'''
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
