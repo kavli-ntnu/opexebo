1a96009d417d65e7625b5e9a3a4f9eeac104dfcc
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2020-08-28 13:09:20 +0200

    Add calc_speed
    * Add a function to calculate animal speed from a list of positions, times
    * Add unit tests for calc_speed
    * Add calc_speed to docs
    * switch `analysis` to import via relative imports

--- opexebo/analysis/__init__.py
+++ opexebo/analysis/__init__.py
@@ -1,28 +1,32 @@
+# Basic functions
+from .speed import calc_speed
+
 # 2D spatial functions : functional
-from opexebo.analysis.spatial_occupancy import spatial_occupancy
-from opexebo.analysis.rate_map import rate_map
-from opexebo.analysis.rate_map_stats import rate_map_stats
-from opexebo.analysis.rate_map_coherence import rate_map_coherence
-from opexebo.analysis.place_field import place_field
-from opexebo.analysis.autocorrelation import autocorrelation
-from opexebo.analysis.grid_score import grid_score, grid_score_stats
-from opexebo.analysis.egocentric_occupancy import egocentric_occupancy
+from .spatial_occupancy import spatial_occupancy
+from .rate_map import rate_map
+from .rate_map_stats import rate_map_stats
+from .rate_map_coherence import rate_map_coherence
+from .place_field import place_field
+from .autocorrelation import autocorrelation
+from .grid_score import grid_score, grid_score_stats
+from .egocentric_occupancy import egocentric_occupancy
 
 # 1D angular functions : functional
-from opexebo.analysis.angular_occupancy import angular_occupancy
-from opexebo.analysis.tuning_curve import tuning_curve
-from opexebo.analysis.tuning_curve_stats import tuning_curve_stats
+from .angular_occupancy import angular_occupancy
+from .tuning_curve import tuning_curve
+from .tuning_curve_stats import tuning_curve_stats
 
 # Miscellanious
-from opexebo.analysis.population_vector_correlation import population_vector_correlation
-from opexebo.analysis.theta_modulation_index import theta_modulation_index
+from .population_vector_correlation import population_vector_correlation
+from .theta_modulation_index import theta_modulation_index
 
 # Experimental
-from opexebo.analysis.speed_score import speed_score
-from opexebo.analysis.border_coverage import border_coverage
-from opexebo.analysis.border_score import border_score
+from .speed_score import speed_score
+from .border_coverage import border_coverage
+from .border_score import border_score
 
-__all__ = ["spatial_occupancy", "rate_map", "rate_map_stats", "rate_map_coherence",
+__all__ = ["calc_speed", 
+        "spatial_occupancy", "rate_map", "rate_map_stats", "rate_map_coherence",
         "grid_score", "grid_score_stats", "autocorrelation", "place_field", 
         "egocentric_occupancy",
            "angular_occupancy", "tuning_curve", "tuning_curve_stats", 
