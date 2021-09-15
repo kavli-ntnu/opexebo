cad22ff642948f93d87075daf615ea42d3b4cf4b
simon-ball <simon.ball@ntnu.no>
2021-09-15 15:30:57 +0200

    bumpversion 0.6.1

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     include_package_data=True,
     keywords='neuroscience kavli gridscore',
     packages=find_packages(include=['opexebo*']),
-    version='0.6.0',
+    version='0.6.1',
     zip_safe=False,
 )
