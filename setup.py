2c0105925431e64111d5cc55ac8389bf029bb47d
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2023-10-04 11:20:23 +0200

    bumpversion to 0.7.0
    Reflecting change of supported Python versions

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     include_package_data=True,
     keywords='neuroscience kavli gridscore',
     packages=find_packages(include=['opexebo*']),
-    version='0.6.2',
+    version='0.7.0',
     zip_safe=False,
 )
